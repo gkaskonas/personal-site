cc0b638da12e3c3f00b1b046a39db1be17bb51ea
Giedrius Kaskonas <pkpersonal@protonmail.com>
2024-05-17 22:09:45 +0100

    fx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,21 @@ import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Peter Kaskonas",
-  description: "Peter Kaskonas personal website",
-  url: "https://peterkaskonas.com",
+  description:
+    "Peter Kaskonas' personal website. Software engineer, AWS Certified Solutions Architect Professional, and AWS Certified DevOps Engineer Professional.",
+  metadataBase: new URL("https://peterkaskonas.com"),
+  alternates: {
+    canonical: "/",
+  },
 };
 
 export default function RootLayout({
