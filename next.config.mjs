47cfaf0ac864ef601c2e913102dbc9d89813dcb4
Giedrius Kaskonas <pkpersonal@protonmail.com>
2024-05-21 20:44:21 +0100

    add sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "pkpersonal",
+  project: "javascript-nextjs",
+
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+
+  transpileClientSDK: true,
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
